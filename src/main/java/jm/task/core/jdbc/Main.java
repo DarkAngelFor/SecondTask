779d6895f8364c097db53fc7a31c5d157a323aa4
DarkAngelFor <rodion-galiev2001@mail.ru>
2024-06-01 13:46:12 +0500

    add logging

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.saveUser("Эмиль", "Галиев", (byte) 25);
+        System.out.println(userDaoJDBC.getAllUsers());
     }
 }
